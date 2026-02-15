1bfb41c9437247825d686aa2fbbffa530e02a97a
Jaime Lopez <jaime.lopez@datainquiry.dev>
2026-02-14 23:11:50 -0500

    removed c++20 features

--- include/colorpalette.h
+++ include/colorpalette.h
@@ -7,193 +7,133 @@
 namespace ColorPalette {
 
 struct ThemeColors {
-  QColor background;
-  QColor backgroundAlt;
-  QColor backgroundCode;
-  QColor backgroundCodeBlock;
-  QColor backgroundLatexInline;
-  QColor backgroundLatexBlock;
-  QColor backgroundInclusion;
-  QColor backgroundInclusionBroken;
-  QColor backgroundSelection;
-  QColor backgroundCurrentLine;
+  QColor background = QColor(252, 250, 248);
+  QColor backgroundAlt = QColor(247, 243, 240);
+  QColor backgroundCode = QColor(245, 240, 245);
+  QColor backgroundCodeBlock = QColor(242, 237, 242);
+  QColor backgroundLatexInline = QColor(250, 240, 250);
+  QColor backgroundLatexBlock = QColor(245, 235, 245);
+  QColor backgroundInclusion = QColor(240, 250, 240);
+  QColor backgroundInclusionBroken = QColor(255, 240, 240);
+  QColor backgroundSelection = QColor(200, 220, 255);
+  QColor backgroundCurrentLine = QColor(255, 255, 230);
   
-  QColor text;
-  QColor textSecondary;
-  QColor textSelection;
-  QColor textDisabled;
+  QColor text = QColor(70, 70, 80);
+  QColor textSecondary = QColor(130, 130, 140);
+  QColor textSelection = QColor(40, 40, 50);
+  QColor textDisabled = QColor(180, 180, 185);
   
-  QColor header;
-  QColor link;
-  QColor wikiLink;
-  QColor brokenLink;
-  QColor code;
-  QColor listMarker;
-  QColor blockquote;
-  QColor latex;
-  QColor strikethrough;
-  QColor inclusion;
-  QColor inclusionBroken;
-  QColor subtleMarkup;
+  QColor header = QColor(100, 120, 200);
+  QColor link = QColor(120, 140, 230);
+  QColor wikiLink = QColor(80, 180, 120);
+  QColor brokenLink = QColor(220, 100, 110);
+  QColor code = QColor(180, 100, 140);
+  QColor listMarker = QColor(100, 170, 100);
+  QColor blockquote = QColor(140, 140, 150);
+  QColor latex = QColor(170, 130, 100);
+  QColor strikethrough = QColor(160, 160, 165);
+  QColor inclusion = QColor(70, 160, 90);
+  QColor inclusionBroken = QColor(200, 80, 90);
+  QColor subtleMarkup = QColor(180, 180, 190);
   
-  QColor syntaxKeyword;
-  QColor syntaxString;
-  QColor syntaxComment;
-  QColor syntaxNumber;
-  QColor syntaxFunction;
-  QColor syntaxType;
+  QColor syntaxKeyword = QColor(140, 110, 200);
+  QColor syntaxString = QColor(110, 170, 120);
+  QColor syntaxComment = QColor(150, 150, 160);
+  QColor syntaxNumber = QColor(210, 150, 110);
+  QColor syntaxFunction = QColor(120, 150, 210);
+  QColor syntaxType = QColor(180, 140, 110);
   
-  QColor border;
-  QColor borderTable;
-  QColor hr;
+  QColor border = QColor(220, 215, 225);
+  QColor borderTable = QColor(215, 210, 220);
+  QColor hr = QColor(210, 205, 215);
   
-  QColor uiWindow;
-  QColor uiWindowText;
-  QColor uiBase;
-  QColor uiAlternateBase;
-  QColor uiButton;
-  QColor uiButtonText;
-  QColor uiButtonTextDisabled;
-  QColor uiHighlight;
-  QColor uiHighlightedText;
-  QColor uiToolTipBase;
-  QColor uiToolTipText;
+  QColor uiWindow = QColor(245, 243, 248);
+  QColor uiWindowText = QColor(70, 70, 80);
+  QColor uiBase = QColor(252, 250, 248);
+  QColor uiAlternateBase = QColor(247, 243, 240);
+  QColor uiButton = QColor(242, 240, 245);
+  QColor uiButtonText = QColor(70, 70, 80);
+  QColor uiButtonTextDisabled = QColor(180, 180, 185);
+  QColor uiHighlight = QColor(150, 170, 230);
+  QColor uiHighlightedText = QColor(255, 255, 255);
+  QColor uiToolTipBase = QColor(255, 255, 235);
+  QColor uiToolTipText = QColor(70, 70, 80);
   
-  QColor lineNumberBackground;
-  QColor lineNumberText;
+  QColor lineNumberBackground = QColor(245, 243, 248);
+  QColor lineNumberText = QColor(150, 150, 160);
   
-  QColor statusSuccess;
-  QColor statusError;
-  QColor statusWarning;
-  QColor statusInfo;
+  QColor statusSuccess = QColor(110, 180, 130);
+  QColor statusError = QColor(220, 120, 130);
+  QColor statusWarning = QColor(230, 170, 110);
+  QColor statusInfo = QColor(130, 160, 220);
 };
 
 inline const ThemeColors& getLightTheme() {
-  static ThemeColors light{
-    .background = QColor(252, 250, 248),
-    .backgroundAlt = QColor(247, 243, 240),
-    .backgroundCode = QColor(245, 240, 245),
-    .backgroundCodeBlock = QColor(242, 237, 242),
-    .backgroundLatexInline = QColor(250, 240, 250),
-    .backgroundLatexBlock = QColor(245, 235, 245),
-    .backgroundInclusion = QColor(240, 250, 240),
-    .backgroundInclusionBroken = QColor(255, 240, 240),
-    .backgroundSelection = QColor(200, 220, 255),
-    .backgroundCurrentLine = QColor(255, 255, 230),
-    
-    .text = QColor(70, 70, 80),
-    .textSecondary = QColor(130, 130, 140),
-    .textSelection = QColor(40, 40, 50),
-    .textDisabled = QColor(180, 180, 185),
-    
-    .header = QColor(100, 120, 200),
-    .link = QColor(120, 140, 230),
-    .wikiLink = QColor(80, 180, 120),
-    .brokenLink = QColor(220, 100, 110),
-    .code = QColor(180, 100, 140),
-    .listMarker = QColor(100, 170, 100),
-    .blockquote = QColor(140, 140, 150),
-    .latex = QColor(170, 130, 100),
-    .strikethrough = QColor(160, 160, 165),
-    .inclusion = QColor(70, 160, 90),
-    .inclusionBroken = QColor(200, 80, 90),
-    .subtleMarkup = QColor(180, 180, 190),
-    
-    .syntaxKeyword = QColor(140, 110, 200),
-    .syntaxString = QColor(110, 170, 120),
-    .syntaxComment = QColor(150, 150, 160),
-    .syntaxNumber = QColor(210, 150, 110),
-    .syntaxFunction = QColor(120, 150, 210),
-    .syntaxType = QColor(180, 140, 110),
-    
-    .border = QColor(220, 215, 225),
-    .borderTable = QColor(215, 210, 220),
-    .hr = QColor(210, 205, 215),
-    
-    .uiWindow = QColor(245, 243, 248),
-    .uiWindowText = QColor(70, 70, 80),
-    .uiBase = QColor(252, 250, 248),
-    .uiAlternateBase = QColor(247, 243, 240),
-    .uiButton = QColor(242, 240, 245),
-    .uiButtonText = QColor(70, 70, 80),
-    .uiButtonTextDisabled = QColor(180, 180, 185),
-    .uiHighlight = QColor(150, 170, 230),
-    .uiHighlightedText = QColor(255, 255, 255),
-    .uiToolTipBase = QColor(255, 255, 235),
-    .uiToolTipText = QColor(70, 70, 80),
-    
-    .lineNumberBackground = QColor(245, 243, 248),
-    .lineNumberText = QColor(150, 150, 160),
-    
-    .statusSuccess = QColor(110, 180, 130),
-    .statusError = QColor(220, 120, 130),
-    .statusWarning = QColor(230, 170, 110),
-    .statusInfo = QColor(130, 160, 220)
-  };
+  static ThemeColors light;
   return light;
 }
 
 inline const ThemeColors& getDarkTheme() {
   static ThemeColors dark{
-    .background = QColor(40, 40, 45),
-    .backgroundAlt = QColor(50, 50, 55),
-    .backgroundCode = QColor(55, 50, 60),
-    .backgroundCodeBlock = QColor(50, 47, 55),
-    .backgroundLatexInline = QColor(60, 50, 65),
-    .backgroundLatexBlock = QColor(58, 48, 63),
-    .backgroundInclusion = QColor(50, 65, 55),
-    .backgroundInclusionBroken = QColor(70, 50, 55),
-    .backgroundSelection = QColor(80, 100, 140),
-    .backgroundCurrentLine = QColor(60, 60, 65),
+    QColor(40, 40, 45),
+    QColor(50, 50, 55),
+    QColor(55, 50, 60),
+    QColor(50, 47, 55),
+    QColor(60, 50, 65),
+    QColor(58, 48, 63),
+    QColor(50, 65, 55),
+    QColor(70, 50, 55),
+    QColor(80, 100, 140),
+    QColor(60, 60, 65),
     
-    .text = QColor(220, 215, 225),
-    .textSecondary = QColor(170, 165, 180),
-    .textSelection = QColor(245, 245, 250),
-    .textDisabled = QColor(120, 120, 125),
+    QColor(220, 215, 225),
+    QColor(170, 165, 180),
+    QColor(245, 245, 250),
+    QColor(120, 120, 125),
     
-    .header = QColor(200, 180, 255),
-    .link = QColor(180, 200, 255),
-    .wikiLink = QColor(150, 230, 200),
-    .brokenLink = QColor(255, 160, 180),
-    .code = QColor(240, 180, 210),
-    .listMarker = QColor(180, 220, 180),
-    .blockquote = QColor(180, 180, 190),
-    .latex = QColor(250, 230, 210),
-    .strikethrough = QColor(160, 160, 170),
-    .inclusion = QColor(160, 240, 200),
-    .inclusionBroken = QColor(240, 150, 170),
-    .subtleMarkup = QColor(120, 120, 130),
+    QColor(200, 180, 255),
+    QColor(180, 200, 255),
+    QColor(150, 230, 200),
+    QColor(255, 160, 180),
+    QColor(240, 180, 210),
+    QColor(180, 220, 180),
+    QColor(180, 180, 190),
+    QColor(250, 230, 210),
+    QColor(160, 160, 170),
+    QColor(160, 240, 200),
+    QColor(240, 150, 170),
+    QColor(120, 120, 130),
     
-    .syntaxKeyword = QColor(210, 180, 240),
-    .syntaxString = QColor(180, 220, 190),
-    .syntaxComment = QColor(130, 130, 145),
-    .syntaxNumber = QColor(240, 200, 170),
-    .syntaxFunction = QColor(180, 200, 240),
-    .syntaxType = QColor(240, 210, 170),
+    QColor(210, 180, 240),
+    QColor(180, 220, 190),
+    QColor(130, 130, 145),
+    QColor(240, 200, 170),
+    QColor(180, 200, 240),
+    QColor(240, 210, 170),
     
-    .border = QColor(80, 80, 90),
-    .borderTable = QColor(85, 85, 95),
-    .hr = QColor(90, 90, 100),
+    QColor(80, 80, 90),
+    QColor(85, 85, 95),
+    QColor(90, 90, 100),
     
-    .uiWindow = QColor(50, 50, 55),
-    .uiWindowText = QColor(220, 215, 225),
-    .uiBase = QColor(40, 40, 45),
-    .uiAlternateBase = QColor(50, 50, 55),
-    .uiButton = QColor(55, 55, 60),
-    .uiButtonText = QColor(220, 215, 225),
-    .uiButtonTextDisabled = QColor(120, 120, 125),
-    .uiHighlight = QColor(130, 150, 210),
-    .uiHighlightedText = QColor(255, 255, 255),
-    .uiToolTipBase = QColor(35, 35, 40),
-    .uiToolTipText = QColor(220, 215, 225),
+    QColor(50, 50, 55),
+    QColor(220, 215, 225),
+    QColor(40, 40, 45),
+    QColor(50, 50, 55),
+    QColor(55, 55, 60),
+    QColor(220, 215, 225),
+    QColor(120, 120, 125),
+    QColor(130, 150, 210),
+    QColor(255, 255, 255),
+    QColor(35, 35, 40),
+    QColor(220, 215, 225),
     
-    .lineNumberBackground = QColor(50, 50, 55),
-    .lineNumberText = QColor(150, 150, 160),
+    QColor(50, 50, 55),
+    QColor(150, 150, 160),
     
-    .statusSuccess = QColor(150, 220, 180),
-    .statusError = QColor(255, 180, 190),
-    .statusWarning = QColor(250, 210, 160),
-    .statusInfo = QColor(170, 190, 240)
+    QColor(150, 220, 180),
+    QColor(255, 180, 190),
+    QColor(250, 210, 160),
+    QColor(170, 190, 240)
   };
   return dark;
 }
