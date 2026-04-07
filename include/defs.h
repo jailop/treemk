c6ec1bf62ade70958914cf3e7de8b42ab3a91647
Jaime Lopez <jaime.lopez@datainquiry.dev>
2026-04-07 10:05:38 -0400

    update

--- include/defs.h
+++ include/defs.h
@@ -2,7 +2,7 @@
 #define DEFS_H
 
 constexpr char APP_LABEL[] = "TreeMk";
-constexpr char APP_VERSION[] = "0.7.0";
+constexpr char APP_VERSION[] = "0.8-beta";
 constexpr int DEFAULT_LINK_SEARCH_DEPTH = 2;
 constexpr int MIN_LINK_SEARCH_DEPTH = 0;
 constexpr int MAX_LINK_SEARCH_DEPTH = 10;
